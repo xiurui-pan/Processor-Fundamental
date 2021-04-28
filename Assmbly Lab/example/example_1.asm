d89977198ee5aa120495980011430966a68b76b8
Xiurui Pan <panxiurui@outlook.com>
2021-04-28 09:13:04 +0800

    fixed encoding

--- Assmbly Lab/example/example_1.asm
+++ Assmbly Lab/example/example_1.asm
@@ -4,9 +4,9 @@
     
 .text
 main:
-    la $a0 string #�����ַ�����ַ
-    li $v0 4      #4������ӡ�ַ���
-    syscall       #ִ��ϵͳ����
+    la $a0 string #载入字符串地址
+    li $v0 4      #4代表打印字符串
+    syscall       #执行系统调用
 
-    li $v0 17     #17����exit
-    syscall       #ִ��ϵͳ����
+    li $v0 17     #17代表exit
+    syscall       #执行系统调用
