d563d728d52f220ab4bde7b899f81ff676437344
DreamDraw Pan <panxiurui@outlook.com>
2021-04-29 15:52:00 +0800

    finished exp1!

--- Assmbly Lab/experiment_1/exp_1_3.asm
+++ Assmbly Lab/experiment_1/exp_1_3.asm
@@ -6,18 +6,58 @@ main:
     syscall
     move $s0, $v0
 
-    sll $t0, $s0, 2
-    sub $sp, $sp, $t0
+    sll $t0, $s0, 2     #dynamically allocate heap memory for array
+    move $a0, $t0
+    li $v0, 9
+    syscall
+    move $s1, $v0       #address of allocated mem stored in s1
+
     li $t1, 0   #iterator in t1
-    for:
-        bge $t1, $s0, endinput
+    for1:
+        bge $t1, $s0, endfor1
         li $v0, 5
         syscall
         move $t0, $v0
 
-        sll $t2, t1, 2  #compute bias for array index, in t2
-        add $a0, $t2, $sp   #compute address for site to be stored, in a0
+        sll $t2, $t1, 2     #compute bias for array index, in t2
+        add $a0, $s1, $t2   #compute address for site to be stored, in a0
         sw $t0, ($a0)
-        j for
+        addi $t1, 1
+        j for1
 
-    endinput:
+    endfor1:
+    li $t1, 0           #iterator in t1
+    srl $s3, $s0, 1     #n/2 stored in s3
+
+    for2:
+        bge $t1, $s3, endfor2
+        sll $t2, $t1, 2     #address of a[i] in a0
+        add $a0, $s1, $t2
+
+        sub $t2, $s0, $t1   #address of a[n-i-1] in a1
+        addi $t2, -1
+        sll $t2, $t2, 2
+        add $a1, $s1, $t2
+
+        lw $t0, ($a0)
+        lw $t2, ($a1)
+        sw $t0, ($a1)
+        sw $t2, ($a0)
+        addi $t1, 1
+        j for2
+
+    endfor2:
+    li $t1, 0   #iterator in t1
+    for3:
+        bge $t1, $s0, endfor3
+        sll $t2, $t1, 2
+        add $a1, $s1, $t2
+        lw $a0, ($a1)
+        li $v0, 1
+        syscall 
+        addi $t1, 1
+        j for3
+
+    endfor3:
+    jr $ra
+
