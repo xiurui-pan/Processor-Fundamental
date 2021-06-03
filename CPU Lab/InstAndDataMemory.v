b5015371a19fd150210aedc20893e27dcc49f7a5
DreamDraw Pan <panxiurui@outlook.com>
2021-06-03 08:59:20 +0800

    finished!

--- CPU Lab/InstAndDataMemory.v
+++ CPU Lab/InstAndDataMemory.v
@@ -47,32 +47,44 @@ module InstAndDataMemory(reset, clk, Address, Write_data, MemRead, MemWrite, Mem
 	always @(posedge reset or posedge clk) begin
 		if (reset) begin
 		    // init instruction memory
-            // addi $a0, $zero, 12123 #(0x2f5b)
-            RAM_data[8'd0] <= {6'h08, 5'd0 , 5'd4 , 16'h2f5b};
-            // addiu $a1, $zero, -12345 #(0xcfc7)
-            RAM_data[8'd1] <= {6'h09, 5'd0 , 5'd5 , 16'hcfc7};
-            // sll $a2, $a1, 16
-            RAM_data[8'd2] <= {6'h00, 5'd0 , 5'd5 , 5'd6 , 5'd16 , 6'h00};
-            // sra $a3, $a2, 16
-            RAM_data[8'd3] <= {6'h00, 5'd0 , 5'd6 , 5'd7 , 5'd16 , 6'h03};
-            // beq $a3, $a1, L1
-            RAM_data[8'd4] <= {6'h04, 5'd7 , 5'd5 , 16'h0001};
-            // lui $a0, 22222 #(0x56ce)
-            RAM_data[8'd5] <= {6'h0f, 5'd0 , 5'd4 , 16'h56ce};
-            // L1:
-            // add $t0, $a2, $a0
-            RAM_data[8'd6] <= {6'h00, 5'd6 , 5'd4 , 5'd8 , 5'd0 , 6'h20};
-            // sra $t1, $t0, 8
-            RAM_data[8'd7] <= {6'h00, 5'd0 , 5'd8 , 5'd9 , 5'd8 , 6'h03};
-            // addi $t2, $zero, -12123 #(0xd0a5)
-            RAM_data[8'd8] <= {6'h08, 5'd0 , 5'd10, 16'hd0a5};
-            // slt $v0, $a0, $t2
-            RAM_data[8'd9] <= {6'h00, 5'd4 , 5'd10 , 5'd2 , 5'd0 , 6'h2a};
-            // sltu $v1, $a0, $t2
-            RAM_data[8'd10] <= {6'h00, 5'd4 , 5'd10 , 5'd3 , 5'd0 , 6'h2b};
-            // Loop:
-            // j Loop
-            RAM_data[8'd11] <= {6'h02, 26'd11};
+            //0 : addi $a0, $zero, 5 
+            RAM_data[8'd0] <= {6'h08 , 5'd0 , 5'd4 , 16'h5};
+            //1 : xor $v0, $zero, $zero 
+            RAM_data[8'd1] <= {6'h00 , 5'd0 , 5'd0 , 5'd2 , 5'd0 , 6'h26};
+            //2 : jal sum 
+            RAM_data[8'd2] <= {6'h03 , 26'h0000004};
+            //Loop: 3 : beq $zero, $zero, Loop 
+            RAM_data[8'd3] <= {6'h04 , 5'd0 , 5'd0 , 16'hffff};
+            //sum:  4 : addi $sp, $sp, -8 
+            RAM_data[8'd4] <= {6'h08 , 5'd29 , 5'd29 , 16'hfff8};
+            //5 : sw $ra, 4($sp) 
+            RAM_data[8'd5] <= {6'h2b , 5'd29 , 5'd31 , 16'd4};
+            //6 : sw $a0, 0($sp) 
+            RAM_data[8'd6] <= {6'h2b , 5'd29 , 5'd4 , 16'd0};
+            //7 : slti $t0, $a0, 1 
+            RAM_data[8'd7] <= {6'h0a , 5'd4 , 5'd8 , 16'd1};
+            //8 : beq $t0, $zero, L1 
+            RAM_data[8'd8] <= {6'h04 , 5'd8 , 5'd0 , 16'h2};
+            //9 : addi $sp, $sp, 8 
+            RAM_data[8'd9] <= {6'h08 , 5'd29 , 5'd29 , 16'd8};
+            //10 : jr $ra 
+            RAM_data[8'd10] <= {6'h00 , 5'd31 , 15'd0 , 6'h08};
+            //L1: 11 : add $v0, $a0, $v0 
+            RAM_data[8'd11] <= {6'h00 , 5'd4 , 5'd2 , 5'd2 , 5'd0 , 6'h20}; 
+            //12 : addi $a0, $a0, -1 
+            RAM_data[8'd12] <= {6'h08 , 5'd4 , 5'd4 , 16'hffff};
+            //13 : jal sum 
+            RAM_data[8'd13] <= {6'h03 , 26'd4};
+            //14 : lw $a0, 0($sp)
+            RAM_data[8'd14] <= {6'h23 , 5'd29 , 5'd4 , 16'd0};
+            //15: lw $ra, 4($sp) 
+            RAM_data[8'd15] <= {6'h23 , 5'd29 , 5'd31 , 16'd4};
+            //16 : addi $sp, $sp, 8 
+            RAM_data[8'd16] <= {6'h08 , 5'd29 , 5'd29 , 16'd8};
+            //17 : add $v0, $a0, $v0 
+            RAM_data[8'd17] <= {6'h00 , 5'd4 , 5'd2 , 5'd2 , 5'd0 , 6'h20}; 
+            //18 : jr $ra 
+            RAM_data[8'd18] <= {6'h00 , 5'd31 , 15'd0 , 6'h08};
        
             //init instruction memory
             //reset data memory		  
--- CPU Lab/RegisterFile.v
+++ CPU Lab/RegisterFile.v
@@ -45,7 +45,10 @@ module RegisterFile(reset, clk, RegWrite, Read_register1, Read_register2, Write_
 	always @(posedge reset or posedge clk) begin
 		if (reset) begin
 			for (i = 1; i < 32; i = i + 1) begin
-				RF_data[i] <= 32'h00000000;
+				if(i == 29)
+					RF_data[i] <= 32'h3fc;
+				else
+					RF_data[i] <= 32'h00000000;
 			end
 		end else if (RegWrite && (Write_register != 5'b00000)) begin
 			RF_data[Write_register] <= Write_data;
